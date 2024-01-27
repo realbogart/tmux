cfba7c43b52ecbb3f907791648480a436c7e5ab8
Johan Yngman <johan.yngman@gmail.com>
2024-01-27 15:08:58 +0100

    Fix preview of ~ for tmux-telescope

--- tmux-telescope/scripts/preview_session.sh
+++ tmux-telescope/scripts/preview_session.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 session_name="$1"
+session_name_escaped=$(printf '%q' "$1")
 
-if tmux has-session -t "$session_name" 2>/dev/null; then
-    tmux capture-pane -pe -t "$session_name"
+if tmux has-session -t "$session_name_escaped" 2>/dev/null; then
+    tmux capture-pane -pe -t "$session_name_escaped"
 else
     echo "Session '$session_name' does not exist."
     echo "Select to create it."
